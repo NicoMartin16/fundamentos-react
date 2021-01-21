afac5de8e220187a4f35b79d3bfd0d5671fafcd1
Nicolas martin <nicolasfelipemartin@gmail.com>
2021-01-21 15:54:37 -0500

    Se inicia proyecto react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Variables y constantes
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const nombre = 'Nicolas';
+const apellido = 'Martin';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
